27329bcff0db3e1569e8e74083db27fd7eac9f0d
nivlacc <q23286919@gmail.com>
2021-03-20 14:03:10 -0700

    Update _Amazon.bat

--- _Amazon.bat
+++ _Amazon.bat
@@ -1,1 +1,2 @@
 cmd /k pipenv run python app.py amazon --test
+cmd /k pipenv run python app.py amazon --single-shot --shipping-bypass --checkshipping --delay=0.5 --p 3 --headless --disable-presence --disable-sound
